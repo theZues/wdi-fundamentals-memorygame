2f4969c9fec564afd884cb8fe91a95832beac68b
Jesus Vasquez <jesusvasquez@Jesuss-iMac.local>
2020-01-23 22:01:16 -0600

    created game lojic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!");
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "king";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
