653cf2e452083ce175ee4a000a461117be440259
Jesus Vasquez <jesusvasquez@Jesuss-iMac.local>
2020-01-27 14:41:10 -0600

    finished game

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,6 @@
 
 
-const cards = [
+let cards = [
 {
 	rank: "queen",
 	suit: "hearts",
@@ -26,34 +26,51 @@ const cards = [
 }
 ];
 
-const cardsInPlay = [];
+let cardsInPlay = [];
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++){
+		let cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+			}
+};
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match");
+		alert("You found a match");
 	}
 		else {
-		console.log("sorry, try again");
+		alert("sorry, try again");
 		}
 
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src",cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 
-}
+};
 
 
 
 
 
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+createBoard();
+
+
+
+
 
 
 
